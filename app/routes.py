ec8609b464c785d1ea809dec5a3601c17a2d2437
Kailon120 <timetravelerkailon120@gmail.com>
2024-08-04 15:02:24 -0600

    Segunda actualización routes.py

--- app/routes.py
+++ app/routes.py
@@ -2,7 +2,7 @@ from flask_restful import Resource
 
 class HelloWorld(Resource):
     def get(self):
-        return { 'message': 'Hola mudo desde la API', 'status': 200}
+        return { 'message': 'Hola mundo desde la API', 'status': 200}
     
 class APIRoutes:
     def init_routes(self, api):
